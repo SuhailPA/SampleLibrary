6a17bde9eef9df40a52d4640b120ae82bd2b4cdf
Suhail <suhailabbas23@gmail.com>
2023-02-16 17:15:07 +0530

    Test Changes

--- testlibrary/src/main/java/com/androidproject/testlibrary/TrailActivity.kt
+++ testlibrary/src/main/java/com/androidproject/testlibrary/TrailActivity.kt
@@ -2,11 +2,16 @@ package com.androidproject.testlibrary
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.androidproject.testlibrary.databinding.ActivityTrailBinding
 
 class TrailActivity : AppCompatActivity() {
+    lateinit var binding : ActivityTrailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_trail)
+        binding = ActivityTrailBinding.inflate(layoutInflater)
+        binding.TestActivity
+        setContentView(binding.root)
+
 
     }
 }
