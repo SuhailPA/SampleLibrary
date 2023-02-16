0b31c3424290ee558b1f55ffba85874e8a165981
Suhail <suhailabbas23@gmail.com>
2023-02-16 16:55:44 +0530

    Initial commit

---
+++ testlibrary/src/main/java/com/androidproject/testlibrary/TrailActivity.kt
@@ -0,0 +1,12 @@
+package com.androidproject.testlibrary
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TrailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trail)
+
+    }
+}
