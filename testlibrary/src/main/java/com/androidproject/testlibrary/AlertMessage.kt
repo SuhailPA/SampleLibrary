1d0f05dbc9a765ced46bedf0c97e6e6b1fc5ad0c
Suhail <suhailabbas23@gmail.com>
2023-02-17 18:06:34 +0530

    Click Listener added

--- testlibrary/src/main/java/com/androidproject/testlibrary/AlertMessage.kt
+++ testlibrary/src/main/java/com/androidproject/testlibrary/AlertMessage.kt
@@ -3,6 +3,7 @@ package com.androidproject.testlibrary
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
+import android.widget.Toast
 
 object AlertMessage {
 
@@ -12,7 +13,11 @@ object AlertMessage {
             .setTitle(title)
             .setMessage(message)
             .setPositiveButton("yes", DialogInterface.OnClickListener{
-                    _, _ ->  onPositiveClickCallback.invoke()
+
+                    _, _ ->
+                Toast.makeText(context,"Successfully clicked the data",Toast.LENGTH_SHORT).show()
+                onPositiveClickCallback.invoke()
+
             })
             .show()
     }
--- testlibrary/src/main/java/com/androidproject/testlibrary/TrailActivity.kt
+++ testlibrary/src/main/java/com/androidproject/testlibrary/TrailActivity.kt
@@ -7,18 +7,12 @@ import com.androidproject.testlibrary.databinding.ActivityTrailBinding
 
 class TrailActivity : AppCompatActivity() {
     lateinit var binding : ActivityTrailBinding
-    var view : View ?= null
+   var view : View ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTrailBinding.inflate(layoutInflater)
-        binding.TestActivity
-        view = binding.root
-        returnView(view!!)
         setContentView(binding.root)
 
 
-    }
-    fun returnView(view : View) : View{
-        return view
     }
 }
