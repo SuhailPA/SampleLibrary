b26c5ee43dd0b14b176bbabb1c389f55636019a9
Suhail <suhailabbas23@gmail.com>
2023-02-20 17:57:25 +0530

    Checking the Interface conditions

--- testlibrary/src/main/java/com/androidproject/testlibrary/TestInterface.kt
+++ testlibrary/src/main/java/com/androidproject/testlibrary/TestInterface.kt
@@ -2,5 +2,8 @@ package com.androidproject.testlibrary
 
 interface TestInterface {
     fun getDataOnMuteButtonClick () : String
-    fun onMuteButtonClickEvent (data : String)
+    fun onMuteButtonClickEvent (data : String) {
+        val check = data
+        check.length
+    }
 }
